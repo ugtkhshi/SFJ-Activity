06e5cf644c2b8653d32ea7f622306f704d0df225
Yuuji <ugtkhshi@ymail.com>
2020-02-05 17:04:25 +0800

    added minor fixes.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-    
+function mainForm() {
+    var forms  = document.forms["mainForm"]["job"]["arrival"]["burst"].value;
+    if (forms == "") {
+        alert("Name must be filled out");
+        return false;
+    }
